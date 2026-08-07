81c173e90f8d27964c985c0f8229413f63dd8ae6
Anthony Costarelli <acostare@nrel.gov>
2026-08-07 12:56:43 -0400

    reenable tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,7 +25,7 @@ const TEST_DIR = @__DIR__
 #   - PowerFlows@psy6 does not precompile, so `PFS` is unavailable in the preamble
 const DISABLED_TESTS = Set([
     "test_ac_transmission_security_constrained_models",
-    "test_pf_injection_resolver",
+    "test_native_tapcontrol",
     "test_power_flow_in_the_loop",
     "test_voltage_control_tap_models",
 ])
